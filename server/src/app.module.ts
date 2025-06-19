75fa5e8e8ada2912413c074c44c8a9adb3bb9ae3
Chris Hoefgen <chris@whalesync.com>
2025-06-19 13:48:56 -0700

    enable config for server

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RecordsGateway } from './records.gateway';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService, RecordsGateway],
 })
