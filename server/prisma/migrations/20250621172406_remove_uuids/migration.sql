c7daccfabf735b016a3c8575bab62e2581a0ee0c
Chris Hoefgen <chris@whalesync.com>
2025-06-21 10:26:06 -0700

    reconstruct primary key for user

---
+++ server/prisma/migrations/20250621172406_remove_uuids/migration.sql
@@ -0,0 +1,34 @@
+/*
+  Warnings:
+
+  - The primary key for the `APIToken` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "APIToken" DROP CONSTRAINT "APIToken_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Connection" DROP CONSTRAINT "Connection_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "APIToken" DROP CONSTRAINT "APIToken_pkey",
+ALTER COLUMN "token" DROP DEFAULT,
+ALTER COLUMN "token" SET DATA TYPE TEXT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT,
+ADD CONSTRAINT "APIToken_pkey" PRIMARY KEY ("token");
+
+-- AlterTable
+ALTER TABLE "Connection" ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- AddForeignKey
+ALTER TABLE "APIToken" ADD CONSTRAINT "APIToken_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Connection" ADD CONSTRAINT "Connection_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
