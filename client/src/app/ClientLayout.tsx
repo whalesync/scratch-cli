ef2f8ad6847b56a54d79a201b730effdf6b933c8
Ivan Dimitrov <ivan@whalesync.com>
2025-06-23 19:19:12 +0300

    cleanup

--- client/src/app/ClientLayout.tsx
+++ client/src/app/ClientLayout.tsx
@@ -13,7 +13,7 @@ export default function ClientLayout({
   children: React.ReactNode;
 }) {
   return (
-    <MantineProvider>
+    <MantineProvider defaultColorScheme="light">
       <Notifications />
       <ClerkProvider>
         <ClerkAuthContextProvider>
