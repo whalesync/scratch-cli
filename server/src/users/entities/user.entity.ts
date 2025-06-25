200afc0702ab5b72e5f3ed01342a6f001259c492
Chris Hoefgen <chris@whalesync.com>
2025-06-25 15:00:51 -0700

    show api token and user id

--- server/src/users/entities/user.entity.ts
+++ server/src/users/entities/user.entity.ts
@@ -1,4 +1,5 @@
-import { User as PrismaUser, UserRole } from '@prisma/client';
+import { UserRole } from '@prisma/client';
+import { UserCluster } from 'src/db/cluster-types';
 
 export class User {
   createdAt: Date;
@@ -6,12 +7,17 @@ export class User {
   clerkId: string | null;
   isAdmin: boolean;
   id: string;
+  apiToken?: string;
 
-  constructor(user: PrismaUser) {
+  constructor(user: UserCluster.User) {
     this.id = user.id;
     this.createdAt = user.createdAt;
     this.updatedAt = user.updatedAt;
     this.clerkId = user.clerkId;
     this.isAdmin = user.role === UserRole.ADMIN;
+
+    if (user.apiTokens) {
+      this.apiToken = user.apiTokens.find((token) => token.expiresAt > new Date())?.token;
+    }
   }
 }
