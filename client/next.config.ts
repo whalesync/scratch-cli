bfb93fee05ad1121dc73148508911631168151af
Ivan Dimitrov <ivan@whalesync.com>
2025-06-26 22:49:38 +0000

    [quickfix] fix forgotten port

--- client/next.config.ts
+++ client/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: `${process.env.API_URL || 'http://localhost:3001'}/:path*`,
+        destination: `${process.env.API_URL || 'http://localhost:3000'}/:path*`,
       },
     ];
   },
