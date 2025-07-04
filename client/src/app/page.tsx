2af4ba2fd1f5b0133ecf9a02117f1e7381de7a29
Chris Hoefgen <chris@whalesync.com>
2025-07-04 14:51:33 -0700

    [client] disable dark mode

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
   const router = useRouter();
 
   useEffect(() => {
-    router.push(RouteUrls.connectionsPageUrl);
+    router.push(RouteUrls.snapshotsPageUrl);
   }, [router]);
 
   return (
