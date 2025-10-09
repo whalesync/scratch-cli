b430ca89be092ea16b66a5a7f903e710dca8d14e
Chris Hoefgen <chris@whalesync.com>
2025-10-09 14:17:32 -0700

    [server] fixed module import for job service
    ## Motivation
    
    Previous commit broke the job service
    
    ## Change description
    
    - added missing module import
    
    
    ## Checklists
    
    - [x] Security impact of change has been considered
    - [x] Either no connector code or sync logic is changed, OR the code path is protected by a BOOLEAN feature flag (default off)
    - Data Incident Risk: 
        - [x] None
        - [ ] Low
        - [ ] High

--- server/src/job/job.module.ts
+++ server/src/job/job.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { ScratchpadConfigModule } from 'src/config/scratchpad-config.module';
 import { AuthModule } from '../auth/auth.module';
 import { DbModule } from '../db/db.module';
 import { JobController } from './job.controller';
 import { JobService } from './job.service';
 
 @Module({
-  imports: [AuthModule, DbModule],
+  imports: [AuthModule, DbModule, ScratchpadConfigModule],
   controllers: [JobController],
   providers: [JobService],
   exports: [JobService],
