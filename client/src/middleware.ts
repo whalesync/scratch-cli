64690ff72c0b22ace53d481e9917766556a3674f
Chris Hoefgen <chris@whalesync.com>
2025-11-07 10:25:06 -0800

    [client] maintenance mode page
    ## Motivation
    
    Need a quick and dirty page to put up when Scratch goes offline
    
    ## Change description
    
    - Add middleware redirect to a static maintenance.html page when the MAINTENANCE_MODE_ENABLED environment variable is "true"
    
    ## Checklists
    
    - [x] Security impact of change has been considered
    - Data Incident Risk: 
        - [x] None
        - [ ] Low
        - [ ] High

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,11 +1,11 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextMiddleware, NextRequest } from "next/server";
-import { RouteUrls } from "./utils/route-urls";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from 'next/server';
+import { RouteUrls } from './utils/route-urls';
 
 function createMiddleware(): NextMiddleware {
   const isPublicRoute = createRouteMatcher(RouteUrls.publicRoutePatterns);
 
-  return clerkMiddleware(
+  const clerkMiddlewareFn = clerkMiddleware(
     async (auth, req) => {
       const { userId, redirectToSignIn } = await auth();
 
@@ -13,23 +13,36 @@ function createMiddleware(): NextMiddleware {
         return redirectToSignIn({ returnBackUrl: originalUrl(req) });
       }
     },
-    { debug: false }
+    { debug: false },
   );
+
+  return async (req: NextRequest, event: NextFetchEvent) => {
+    // Check for maintenance mode
+    const maintenanceMode = process.env.MAINTENANCE_MODE_ENABLED === 'true';
+    // prevent infinite redirect loop
+    const isMaintenancePage = req.nextUrl.pathname === '/maintenance.html';
+    if (maintenanceMode && !isMaintenancePage) {
+      return NextResponse.redirect(new URL('/maintenance.html', req.url));
+    }
+
+    // Continue with regular middleware authentication logic
+    return clerkMiddlewareFn(req, event);
+  };
 }
 
 export default createMiddleware();
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
 };
 
 function originalUrl(req: NextRequest): string {
   const url = new URL(req.url);
 
-  const forwardedHost = req.headers.get("x-forwarded-host");
+  const forwardedHost = req.headers.get('x-forwarded-host');
   if (forwardedHost) {
     url.hostname = forwardedHost;
-    url.port = "";
+    url.port = '';
   }
 
   return url.toString();
