0ad41fcc029442a17f602d7cb68196c2bd24650b
Chris Hoefgen <chris@whalesync.com>
2025-11-13 16:24:36 -0800

    [cleanup] remove deprecated environment variable
    ## Motivation
    
    No longer need the logic around making credentials optional/mandatory
    
    ## Change description
    
    - removed env-var and logic using it in the client
    - removed the value from the Dockerfile
    
    ## Checklists
    
    - [x] Security impact of change has been considered
    - Data Incident Risk: 
        - [x] None
        - [ ] Low
        - [ ] High

--- client/src/hooks/use-agent-credentials.ts
+++ client/src/hooks/use-agent-credentials.ts
@@ -1,74 +1,64 @@
-import { agentCredentialsApi } from "@/lib/api/agent-credentials";
-import { isUnauthorizedError } from "@/lib/api/error";
-import { SWR_KEYS } from "@/lib/api/keys";
-import { CreateAiAgentCredentialDto, UpdateAiAgentCredentialDto } from "@/types/server-entities/agent-credentials";
-import { useMemo } from "react";
-import useSWR, { useSWRConfig } from "swr";
-import { useScratchPadUser } from "./useScratchpadUser";
+import { agentCredentialsApi } from '@/lib/api/agent-credentials';
+import { isUnauthorizedError } from '@/lib/api/error';
+import { SWR_KEYS } from '@/lib/api/keys';
+import { CreateAiAgentCredentialDto, UpdateAiAgentCredentialDto } from '@/types/server-entities/agent-credentials';
+import { useMemo } from 'react';
+import useSWR, { useSWRConfig } from 'swr';
 
 export const useAgentCredentials = (includeUsageStats: boolean = false) => {
-    const { mutate } = useSWRConfig();
-    const {user} = useScratchPadUser();
+  const { mutate } = useSWRConfig();
 
-    const { data, error, isLoading, mutate: mutateList } = useSWR(
-      SWR_KEYS.agentCredentials.list(includeUsageStats),
-      () => agentCredentialsApi.list(includeUsageStats)
-    );
-  
-    const createCredentials = async (dto: CreateAiAgentCredentialDto) => {
-      await agentCredentialsApi.create(dto);
-      mutateList();
-    };
-  
-    const updateCredentials = async (id: string, dto: UpdateAiAgentCredentialDto) => {
-      await agentCredentialsApi.update(id, dto);
-      mutateList();
-      mutate(SWR_KEYS.agentCredentials.detail(id));
-    };
+  const {
+    data,
+    error,
+    isLoading,
+    mutate: mutateList,
+  } = useSWR(SWR_KEYS.agentCredentials.list(includeUsageStats), () => agentCredentialsApi.list(includeUsageStats));
 
-    const deleteCredentials = async (id: string) => {
-      await agentCredentialsApi.delete(id);
-      mutateList();
-    };
+  const createCredentials = async (dto: CreateAiAgentCredentialDto) => {
+    await agentCredentialsApi.create(dto);
+    mutateList();
+  };
 
-    const toggleDefaultCredential = async (id: string) => {
-      await agentCredentialsApi.setDefaultKey(id);
-      mutateList();
-      mutate(SWR_KEYS.agentCredentials.detail(id));
-    };
+  const updateCredentials = async (id: string, dto: UpdateAiAgentCredentialDto) => {
+    await agentCredentialsApi.update(id, dto);
+    mutateList();
+    mutate(SWR_KEYS.agentCredentials.detail(id));
+  };
 
-    const activeOpenRouterCredentials = useMemo(() => {
-      // try to find the default credential
-      return data?.find((credential) => credential.default && credential.service === 'openrouter');
+  const deleteCredentials = async (id: string) => {
+    await agentCredentialsApi.delete(id);
+    mutateList();
+  };
 
-    }, [data]);
-  
-    const aiAgentEnabled = useMemo(() => {
-        // if user agent credentials are required, and the user is not an admin, then the AI agent is enabled if the user has active open router credentials
-        const requireUserAgentCredentials = process.env.NEXT_PUBLIC_USER_AGENT_CREDENTIALS_REQUIRED === 'true';
-      if(requireUserAgentCredentials && !user?.isAdmin){
-        return !!activeOpenRouterCredentials;
-      }
-      return true; 
-    }, [activeOpenRouterCredentials, user]);
+  const toggleDefaultCredential = async (id: string) => {
+    await agentCredentialsApi.setDefaultKey(id);
+    mutateList();
+    mutate(SWR_KEYS.agentCredentials.detail(id));
+  };
 
-    const displayError = useMemo(() => {
-      if(isUnauthorizedError(error)) {
-        // ignore this error as it will be fixed after the token is refreshed
-        return undefined;
-      }
-      return error?.message;
-    }, [error]);
-    
-    return {
-      agentCredentials: data,
-      isLoading,
-      error: displayError,
-      createCredentials,
-      updateCredentials,
-      deleteCredentials,
-      aiAgentEnabled,
-      toggleDefaultCredential,
-      activeOpenRouterCredentials
+  const activeOpenRouterCredentials = useMemo(() => {
+    // try to find the default credential
+    return data?.find((credential) => credential.default && credential.service === 'openrouter');
+  }, [data]);
+
+  const displayError = useMemo(() => {
+    if (isUnauthorizedError(error)) {
+      // ignore this error as it will be fixed after the token is refreshed
+      return undefined;
+    }
+    return error?.message;
+  }, [error]);
+
+  return {
+    agentCredentials: data,
+    isLoading,
+    error: displayError,
+    createCredentials,
+    updateCredentials,
+    deleteCredentials,
+    aiAgentEnabled: !!activeOpenRouterCredentials,
+    toggleDefaultCredential,
+    activeOpenRouterCredentials,
   };
 };
