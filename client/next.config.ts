9f6ea172568348ac4e839634056781795b9c1b54
Chris Hoefgen <chris@whalesync.com>
2025-11-25 14:50:04 -0800

    [cicd, client] rework sourcemap upload to posthog
    ## Motivation
    
    The previous implementation was the incorrect way to do it for Next.js. 
    
    ## Change description
    
    - removed the gitlab job to upload source maps
    - added @posthog/next-config package
    - New Next.js config that will do source map uploads for prod builds on Test and Production envs.
    - Updated dockerfiles to include new posthog env-vars during build
    - Updated docker build jobs to provide the posthog env-vars
    
    ## Checklists
    
    - [x] Security impact of change has been considered
    - Data Incident Risk: 
        - [x] None
        - [ ] Low
        - [ ] High

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,11 +1,27 @@
+import { BUILD_VERSION } from '@/version';
+import { withPostHogConfig } from '@posthog/nextjs-config';
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
+let nextConfig: NextConfig = {
   output: 'standalone',
   productionBrowserSourceMaps: true,
   devIndicators: {
     position: 'bottom-left',
   },
+};
+
+if(process.env.POSTHOG_PROJECT_ID && process.env.POSTHOG_API_KEY) {
+  // Additional config to upload sourcemaps to PostHog for production builds
+  nextConfig = withPostHogConfig(nextConfig, {
+  envId: process.env.POSTHOG_PROJECT_ID as string, // Environment ID for the project
+  personalApiKey: process.env.POSTHOG_API_KEY as string, // Personal API key for uploading sourcemaps
+  host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  sourcemaps: {
+    enabled: true,
+    version: BUILD_VERSION || undefined,
+    deleteAfterUpload: true,
+  },
+});
 };
 
 export default nextConfig;
