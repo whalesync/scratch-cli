5efe99ff8b2ddf94155553c4db71b0e6b8445f20
Lucio Cesolari <lucio@whalesync.com>
2025-12-17 21:36:14 +0000

    [client] Tooltip for change dots component.
    ## Motivation
    
    ![image](/uploads/d3f8b8a0e283a6b803c4c9e90e7e3cc6/image.png){width=681 height=408}
    ![image](/uploads/f25fad4b96e23864f6e36028d2f1eb3e/image.png){width=491 height=402}
    
    ## Change description
    
    * [client] Tooltip for change dots component.
    
    ## Checklists
    
    - [x] Security impact of change has been considered
    - Data Incident Risk: 
        - [x] None
        - [ ] Low
        - [ ] High

--- client/src/app/components/field-value-wrappers/ChangeDotsGroup/ChangeDotsGroup.tsx
+++ client/src/app/components/field-value-wrappers/ChangeDotsGroup/ChangeDotsGroup.tsx
@@ -1,28 +1,37 @@
-import { Box, Group } from '@mantine/core';
+import { Box, Group, Tooltip } from '@mantine/core';
 import { FC } from 'react';
 import { ExistingChangeTypes, getChangeTypeColors } from '../ProcessedFieldValue';
 import styles from './ChangeDotsGroup.module.css';
+
 type Props = {
   changeTypes: ExistingChangeTypes;
 };
 export const ChangeDotsGroup: FC<Props> = (props) => {
   const changeColors = getChangeTypeColors(props.changeTypes);
+  let tooltipText = '';
+  if (props.changeTypes.suggestedAdditions || props.changeTypes.suggestedDeletions) {
+    tooltipText = 'Suggested changes';
+  } else {
+    tooltipText = 'Unpublished changes';
+  }
   return (
-    <Group gap={4} className={styles.dotGroup}>
-      <Box
-        className={styles.cellComponentAnyDot}
-        style={{
-          backgroundColor: changeColors.deletionColor,
-          boxShadow: changeColors.deletionShadow,
-        }}
-      ></Box>
-      <Box
-        className={styles.cellComponentAnyDot}
-        style={{
-          backgroundColor: changeColors.additionColor,
-          boxShadow: changeColors.additionShadow,
-        }}
-      ></Box>
-    </Group>
+    <Tooltip label={tooltipText} position="top" withArrow>
+      <Group gap={4} className={styles.dotGroup}>
+        <Box
+          className={styles.cellComponentAnyDot}
+          style={{
+            backgroundColor: changeColors.deletionColor,
+            boxShadow: changeColors.deletionShadow,
+          }}
+        ></Box>
+        <Box
+          className={styles.cellComponentAnyDot}
+          style={{
+            backgroundColor: changeColors.additionColor,
+            boxShadow: changeColors.additionShadow,
+          }}
+        ></Box>
+      </Group>
+    </Tooltip>
   );
 };
